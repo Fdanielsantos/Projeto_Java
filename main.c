50a62105eb8d561d0f47501c0711de5eaf1c7abe
Fdanielsantos <135390234+Fdanielsantos@users.noreply.github.com>
2024-06-17 13:11:31 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "calculadora.h"
+
+int main() {
+    char expr[MAXLEN];
+
+    printf("Digite a expressao pos-fixada: ");
+    fgets(expr, MAXLEN, stdin);
+
+    // Remove a nova linha do final da string
+    expr[strcspn(expr, "\n")] = '\0';
+
+    printf("Avaliando: %s\n", expr);
+    avaliaPosfixada(expr);
+
+    return 0;
+}
